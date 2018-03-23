0b33d19677a48c2c0a469e4b74d6352fd45861d8
Antoha5001 <plantsvzzombi@mail.ru>
2018-03-23 19:16:46 +0700

    123

--- less1/blog/edit.php
+++ less1/blog/edit.php
@@ -7,14 +7,18 @@
 	}
 include_once("functions.php");
 
-	if(count($_GET) >0){
+
+	if(count($_GET) > 0){
+
 		$title = trim($_GET['fname']);
+
+		echo $_GET['title'];
+
 		$content = file_get_contents("data/$title");
 //		$content = trim($_POST['content']);
 
-		if($title == "" || $content == ""){
-		  $msg = "Заполните все поля";
-		}
+
+
 //		elseif (file_exists("data/$title")){
 //
 //		  $msg = "Такая новость уже существует!";
@@ -25,6 +29,31 @@ include_once("functions.php");
 //		  $msg = "Не верный формат!";
 //		}
 
+		if(count($_POST) >0){
+
+			if($title == "" || $content == ""){
+				$msg = "Заполните все поля";
+			}
+
+			$title = trim($_POST['title']);
+
+			if(preg_match("/^[a-z0-9_]+$/m",$title)){
+
+				$content = trim($_POST['content']);
+				$file = fopen("data/$title","w");
+
+				fwrite($file, $content);
+
+				fclose($file);
+				header("Location: edit.php");
+				exit();
+
+			} else{
+				$msg = "Символы не верные в названии используются!";
+			}
+
+		}
+
 
 	}
 	else{
@@ -34,16 +63,7 @@ include_once("functions.php");
 		$msg = "";
 	}
 
-	if(count($_POST) >0){
-		$content = trim($_POST['content']);
-		$file = fopen("data/$title","w");
 
-		fwrite($file, $content);
-
-		fclose($file);
-		header("Location: edit.php");
-		exit();
-	}
 
 	$list = scandir('data');
 
@@ -56,14 +76,21 @@ include_once("functions.php");
 
 
 <form method="post">
-	Название <br>
-	<input type="text" name="title" value="<?php echo $title ?>">
+	<br>Название <br>
+	<input type="text" name="title" value="<?php echo $title ?>"><br>
 	Контент <br>
-	<textarea  id="" cols="30" rows="10" name="content"><?php echo $content ?></textarea>
+	<textarea  id="" cols="30" rows="10" name="content"><?php echo $content ?></textarea><br>
 	<input type="submit" value="Добавить">
 </form>
 <br>
-<a href="index.php">На главную</a>
-<?php echo $msg; ?>
+<?php echo $msg; ?><br><br>
+<a href="index.php">На главную</a><br><br>
+<?php
+
+	echo "data/".$title;
+echo file_exists("data/../index.php");
+echo checkTitle("indexphp");
+echo preg_match("/^[a-z0-9_]+$/m",$title);
+echo $_POST['fname'];
 
 
